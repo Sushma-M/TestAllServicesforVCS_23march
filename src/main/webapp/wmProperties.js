d1d2abb0b2bb4edcf745d70e32770756218ff63d
sushma maddirala <sushma.maddirala@wavemaker.com>
2018-08-29 09:09:43 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,15 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "passion",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "TestAllServicesforVCS_23march",
+  "homePage" : "Main",
+  "name" : "TestAllServicesforVCS_23march",
+  "platformType" : "WEB",
+  "securityEnabled" : "true",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0",
+  "xsrf_header_name" : "X-WM-XSRF-TOKEN"
+};
